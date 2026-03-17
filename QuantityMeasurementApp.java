15ec0d6385e690eedd562fca57c7709eed30ab5d
vishalraj096 <vishal20031singh@gmail.com>
2026-03-17 10:10:31 +0530

    Generic Quantity completed

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -1,78 +1,109 @@
 public class QuantityMeasurementApp {
-    public static class Feet {
-        private final double value;
+    public enum LengthUnit {
+        FEET(1.0),
+        INCHES(1.0 / 12.0);
 
-        public Feet(double value) {
+        private final double toFeetFactor;
+
+        LengthUnit(double toFeetFactor) {
+            this.toFeetFactor = toFeetFactor;
+        }
+
+        public double getToFeetFactor() {
+            return toFeetFactor;
+        }
+
+        public static LengthUnit from(String unitText) {
+            if (unitText == null) {
+                throw new IllegalArgumentException("Unit cannot be null");
+            }
+            String normalized = unitText.trim().toUpperCase();
+            if (normalized.equals("FOOT") || normalized.equals("FEET")) {
+                return FEET;
+            }
+            if (normalized.equals("INCH") || normalized.equals("INCHES")) {
+                return INCHES;
+            }
+            throw new IllegalArgumentException("Unsupported unit: " + unitText);
+        }
+    }
+
+    public static class Length {
+        private static final double EPSILON = 1e-9;
+
+        private final double value;
+        private final LengthUnit unit;
+
+        public Length(double value, LengthUnit unit) {
+            if (!Double.isFinite(value)) {
+                throw new IllegalArgumentException("Length value must be a finite number");
+            }
+            if (unit == null) {
+                throw new IllegalArgumentException("Length unit cannot be null");
+            }
             this.value = value;
+            this.unit = unit;
+        }
+
+        public Length(double value, String unitText) {
+            this(value, LengthUnit.from(unitText));
+        }
+
+        private double toFeet() {
+            return value * unit.getToFeetFactor();
         }
 
         @Override
         public boolean equals(Object obj) {
-            if (this == obj)
+            if (this == obj) {
                 return true;
-            if (obj == null || getClass() != obj.getClass())
+            }
+            if (obj == null || getClass() != obj.getClass()) {
                 return false;
-            Feet temp = (Feet) obj;
-            return Double.compare(this.value, temp.value) == 0;
+            }
+            Length other = (Length) obj;
+            return Math.abs(this.toFeet() - other.toFeet()) < EPSILON;
         }
 
         @Override
         public int hashCode() {
-            return Double.hashCode(value);
+            double normalizedFeet = Math.round(toFeet() / EPSILON) * EPSILON;
+            return Double.hashCode(normalizedFeet);
         }
     }
 
-    public static class Inch {
-        private final double value;
+    public static void demonstrateFeetEquality() {
+        Length obj1 = new Length(6.4, LengthUnit.FEET);
+        Length obj2 = new Length(6.4, LengthUnit.FEET);
 
-        public Inch(double value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null || getClass() != obj.getClass())
-                return false;
-            Inch temp = (Inch) obj;
-            return Double.compare(this.value, temp.value) == 0;
-        }
-
-        @Override
-        public int hashCode() {
-            return Double.hashCode(value);
-        }
-    }
-
-    public static void feetEquality() {
-        double val1 = 6.4;
-        double val2 = 6.4;
-
-        Feet obj1 = new Feet(val1);
-        Feet obj2 = new Feet(val2);
-
-        if (obj1.equals(obj2))
+        if (obj1.equals(obj2)) {
             System.out.println("Both are equal in feet");
-        else
+        } else {
             System.out.println("Values are different");
+        }
     }
 
-    public static void inchEquality() {
-        double val1 = 12.0;
-        double val2 = 12.0;
+    public static void demonstrateInchesEquality() {
+        Length obj1 = new Length(12.0, LengthUnit.INCHES);
+        Length obj2 = new Length(12.0, "inch");
 
-        Inch obj1 = new Inch(val1);
-        Inch obj2 = new Inch(val2);
-
-        if (obj1.equals(obj2))
+        if (obj1.equals(obj2)) {
             System.out.println("Both are equal in inches");
-        else
+        } else {
             System.out.println("Values are different");
+        }
+    }
+
+    public static void demonstrateFeetInchesComparison() {
+        Length obj1 = new Length(1.0, "feet");
+        Length obj2 = new Length(12.0, "inches");
+
+        System.out.println("Are 1 foot and 12 inches equal? " + obj1.equals(obj2));
     }
 
     public static void main(String[] args) {
-        feetEquality();
-        inchEquality();
+        demonstrateFeetEquality();
+        demonstrateInchesEquality();
+        demonstrateFeetInchesComparison();
     }
 }
